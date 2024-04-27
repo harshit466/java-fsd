d2593fa87c782762ecd6255ce244474e60c5a6f4
Harshit Goyal <harshitgoyal466@gmail.com>
2024-04-27 13:36:31 +0530

    Practiceproject

---
+++ src/Phase3/Phase3_Practiceproject3/DempSpringJDBC/src/main/java/com/beans/Product.java
@@ -0,0 +1,33 @@
+package com.beans;
+
+public class Product {
+	private int pid;
+	private String pname;
+	private float price;
+	private int stk;
+	
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public int getStk() {
+		return stk;
+	}
+	public void setStk(int stk) {
+		this.stk = stk;
+	}
+}
